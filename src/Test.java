46538bcaebb6d5cfe6fafc191795ecd0efd25017
AlexsEROKhin <o.k.yerokhin@student.khai.edu>
2021-12-09 22:58:39 +0200

    Test.java added to feature_branch with change on string#3

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,6 @@ public class Test {
     public static void main(String[] args){
         System.out.println("Java education");
         System.out.println("education");
-        //new string
-
+        System.out.println("DICT");
     }
 }
