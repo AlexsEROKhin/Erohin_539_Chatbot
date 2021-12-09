a9dbf6532f5aeb2b06d2f03990c2eebd423ca733
AlexsEROKhin <o.k.yerokhin@student.khai.edu>
2021-12-10 00:01:13 +0200

    CHATBOT STAGE 1

---
+++ src/nix/java/education/chatbot/ChatBot.java
@@ -0,0 +1,9 @@
+package nix.java.education.chatbot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is LehaBot");
+        System.out.println("I was created in 2021");
+    }
+
+}
