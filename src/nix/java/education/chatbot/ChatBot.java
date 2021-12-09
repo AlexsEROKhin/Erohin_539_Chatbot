03f8d42595ad182cd86188ad1b037419efc3773e
AlexsEROKhin <o.k.yerokhin@student.khai.edu>
2021-12-10 00:03:39 +0200

    CHATBOT STAGE 2

--- src/nix/java/education/chatbot/ChatBot.java
+++ src/nix/java/education/chatbot/ChatBot.java
@@ -1,9 +1,17 @@
 package nix.java.education.chatbot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         System.out.println("Hello! My name is LehaBot");
         System.out.println("I was created in 2021");
+        Scanner Scan = new Scanner(System.in);
+        String name;
+
+        System.out.println("Please, remind me your name:");
+        name = Scan.nextLine();
+        System.out.println("What a great name you have," + name);
     }
 
 }
