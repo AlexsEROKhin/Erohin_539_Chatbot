f25bed15c245e29014eb8a0239be2d66ffd6e7b0
AlexsEROKhin <o.k.yerokhin@student.khai.edu>
2021-12-10 00:05:38 +0200

    CHATBOT STAGE 3

--- src/nix/java/education/chatbot/ChatBot.java
+++ src/nix/java/education/chatbot/ChatBot.java
@@ -12,6 +12,17 @@ public class ChatBot {
         System.out.println("Please, remind me your name:");
         name = Scan.nextLine();
         System.out.println("What a great name you have," + name);
+
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        int age1 = Scan.nextInt();
+        int remainder3 = age1 % 3;
+        int age2 = Scan.nextInt();
+        int remainder5 = age2 % 5;
+        int age3 = Scan.nextInt();
+        int remainder7 = age3 % 7;
+
+        System.out.println("Your age is " + (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105 + "," + "that's a good time to start programming!");
+
     }
 
 }
