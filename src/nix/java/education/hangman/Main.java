62f2deef514c0c90c531fe9bf6cc6191333636ef
AlexsEROKhin <o.k.yerokhin@student.khai.edu>
2021-12-21 17:24:09 +0200

    stage 1

---
+++ src/nix/java/education/hangman/Main.java
@@ -0,0 +1,12 @@
+package nix.java.education.hangman;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+                System.out.println("Висилица!\nИгра скоро будет доступна...");
+            }
+}
+
+
