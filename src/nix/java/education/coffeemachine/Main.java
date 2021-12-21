4b27a91a846154259c2448a9fecbb901fc4b8a7f
AlexsEROKhin <o.k.yerokhin@student.khai.edu>
2021-12-21 17:48:42 +0200

    stage 1

---
+++ src/nix/java/education/coffeemachine/Main.java
@@ -0,0 +1,15 @@
+package nix.java.education.coffeemachine;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                Starting to make coffee
+                Grinding coffee beans
+                Boiling water
+                Mixin boiled water with crushed coffee beans
+                Pouring coffee into the cup
+                Pouring some milk into the cup
+                Ingridients is ready!
+                """);
+    }
+}
---
+++ src/nix/java/education/coffeemachine/ingredients.java
@@ -0,0 +1,4 @@
+package nix.java.education.coffeemachine;
+
+public enum ingredients {
+}
