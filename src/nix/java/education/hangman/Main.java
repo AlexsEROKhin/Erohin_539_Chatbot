45841c78b90e28030784c42728c4c3db1d387bf8
AlexsEROKhin <o.k.yerokhin@student.khai.edu>
2021-12-21 17:24:47 +0200

    stage 2

--- src/nix/java/education/hangman/Main.java
+++ src/nix/java/education/hangman/Main.java
@@ -6,7 +6,19 @@ public class Main {
     public static void main(String[] args) {
                 Scanner scan = new Scanner(System.in);
                 System.out.println("Висилица!\nИгра скоро будет доступна...");
-            }
+        System.out.print("Угадай слово: ");
+        String answer = scan.nextLine();
+        String word = "java";
+        if (answer.equals(word)){
+            System.out.println("Вы выжили!");
+        }
+        else{
+            System.out.println("Вы проиграли!");
+        }
+    }
 }
+
+
+
 
 
