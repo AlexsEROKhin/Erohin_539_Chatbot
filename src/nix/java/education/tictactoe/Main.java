16e588a379bc153ace64f33368c2bdbd8b633ee9
AlexsEROKhin <o.k.yerokhin@student.khai.edu>
2021-12-21 18:21:09 +0200

    stage 1

---
+++ src/nix/java/education/tictactoe/Main.java
@@ -0,0 +1,14 @@
+package nix.java.education.tictactoe;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+        X X O
+        O X O
+        X O X
+        """);
+    }
+}
