048abb162d0cb611f46b37cb5996357604d3ec49
AlexsEROKhin <o.k.yerokhin@student.khai.edu>
2021-12-21 18:22:25 +0200

    stage 2

--- src/nix/java/education/tictactoe/Main.java
+++ src/nix/java/education/tictactoe/Main.java
@@ -3,12 +3,9 @@ package nix.java.education.tictactoe;
 import java.util.Scanner;
 
 public class Main {
+    public static final Scanner  scanner =new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("""
-        X X O
-        O X O
-        X O X
-        """);
+        Game ticTacToe = new Game();
+        ticTacToe.createBoard();
     }
 }
