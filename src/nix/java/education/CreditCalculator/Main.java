dfa277dfdfe7fc6584a41cedecb5aa38cee271e2
AlexsEROKhin <o.k.yerokhin@student.khai.edu>
2021-12-21 18:48:35 +0200

    stage 2

--- src/nix/java/education/CreditCalculator/Main.java
+++ src/nix/java/education/CreditCalculator/Main.java
@@ -3,17 +3,9 @@ package nix.java.education.CreditCalculator;
 import java.util.Scanner;
 
 public class Main {
+    public static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        //Stage 1
-
-        System.out.println("""
-                Loan principal: 1000
-                Month 1:repaid 250
-                Month 2:repaid 250
-                Month 3:repaid 500
-                The loan has been repaid
-                """);
+        Calculator calculator =new Calculator();
+        calculator.chooseAction();
     }
 }
