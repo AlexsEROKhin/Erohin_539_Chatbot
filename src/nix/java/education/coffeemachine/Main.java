ef5385e2a294b0917bdce9d994d3f17deb933d28
AlexsEROKhin <o.k.yerokhin@student.khai.edu>
2021-12-21 18:06:07 +0200

    stage 4

--- src/nix/java/education/coffeemachine/Main.java
+++ src/nix/java/education/coffeemachine/Main.java
@@ -1,9 +1,10 @@
 package nix.java.education.coffeemachine;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public static final Scanner scanner = new Scanner(System.in);
+    public static final CoffeeMachine employee = new CoffeeMachine(400,540,120,9, 550.00);
     public static void main(String[] args) {
         System.out.println("""
                 Starting to make coffee
@@ -14,36 +15,6 @@ public class Main {
                 Pouring some milk into the cup
                 Ingridients is ready!
                 """);
-        System.out.println("Write how many cup of coffee you will need:");
-        Scanner scanner = new Scanner(System.in);
-        int cupsOfCoffee = scanner.nextInt();
-        final int water = 200;
-        final int milk = 50;
-        final int coffeeBeams = 15;
-        System.out.println("For" + " " + cupsOfCoffee + " " + "coffee you will need:");
-        System.out.println(water * cupsOfCoffee + " " + "ml of water");
-        System.out.println(milk * cupsOfCoffee + " " + "ml of milk");
-        System.out.println(coffeeBeams * cupsOfCoffee + " " + "g of coffeeBeams");
-        System.out.println("Write how many ml of water you will have:");
-        int amountOfWater = scanner.nextInt();
-        System.out.println("Write many how ml of milk you will have:");
-        int amountOfMilk = scanner.nextInt();
-        System.out.println("Write how many grams of CoffeeBeams you will have:");
-        int amountOfCoffeeBeams = scanner.nextInt();
-        int coffeeForWater = amountOfWater / water;
-        int coffeeForMilk = amountOfMilk / milk;
-        int coffeeForCoffeeBeams = amountOfCoffeeBeams / coffeeBeams;
-        int[] tab = {coffeeForWater, coffeeForMilk, coffeeForCoffeeBeams};
-        int min = Arrays.stream(tab).min().getAsInt();
-        System.out.println("Write how many cup of coffee you will need:");
-        int cupOfCoffee = scanner.nextInt();
-        int N = min - cupOfCoffee;
-        if (min == cupOfCoffee) {
-            System.out.println("Yes, i can make that amount of coffee");
-        } else if (min > cupOfCoffee) {
-            System.out.println("Yes, i can make that amount of coffee(and even" + " " + N + " " + "more than that)");
-        } else {
-            System.out.println("No,i make only" + " " + min + " " + "cups of coffee\n");
-        }
+
     }
 }
--- src/nix/java/education/coffeemachine/ingredients.java
+++
@@ -1,4 +0,0 @@
-package nix.java.education.coffeemachine;
-
-public enum ingredients {
-}
