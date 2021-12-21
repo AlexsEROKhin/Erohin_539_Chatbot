75b15fa3a47597130b79ee834988422905aa4932
AlexsEROKhin <o.k.yerokhin@student.khai.edu>
2021-12-21 18:31:51 +0200

    stage 4

--- src/nix/java/education/tictactoe/Main.java
+++ src/nix/java/education/tictactoe/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Game ticTacToe = new Game();
         ticTacToe.createBoard();
-        ticTacToe.checkWinner();
+        ticTacToe.setCordinates();
     }
 }
