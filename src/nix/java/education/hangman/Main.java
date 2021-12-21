dc9559d55654490b9be3148171579e5122acc32c
AlexsEROKhin <o.k.yerokhin@student.khai.edu>
2021-12-21 17:25:29 +0200

    stage 3

--- src/nix/java/education/hangman/Main.java
+++ src/nix/java/education/hangman/Main.java
@@ -1,5 +1,6 @@
 package nix.java.education.hangman;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -10,6 +11,18 @@ public class Main {
         String answer = scan.nextLine();
         String word = "java";
         if (answer.equals(word)){
+            System.out.println("Вы выжили!");
+        }
+        else{
+            System.out.println("Вы проиграли!");
+
+        }
+        Random random = new Random();
+        String[] words = {"java","python", "javascript", "kotlin"};
+        String variation = words[random.nextInt(words.length)];
+        System.out.print("Раунд 2\nУгадай слово: ");
+        String answer2 = scan.nextLine();
+        if (answer2.equals(variation)){
             System.out.println("Вы выжили!");
         }
         else{
